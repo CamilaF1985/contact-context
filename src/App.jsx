63655b91cfa6a88ad8748c84ab1463a979581538
Camila Fabbroni <camila.fabbroni1985@gmail.com>
2023-11-20 20:50:40 -0300

    menú principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Contactos.jsx
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Contactos = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container mt-5">
+      <div className="text-center mb-4">
+        <h1 className="display-4">Gestor de Contactos</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="row mt-5">
+        <div className="col-md-6">
+          <a href="#" className="btn btn-primary btn-lg w-100">Lista de Contactos</a>
+        </div>
+        <div className="col-md-6">
+          <a href="#" className="btn btn-success btn-lg w-100">Añadir Nuevo Contacto</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contactos;
+
