71c70d95efbe7681e964ba4664e3f17191baa7c0
Camila Fabbroni <camila.fabbroni1985@gmail.com>
2023-11-20 22:32:55 -0300

    primer borrador lista de contactos

--- src/Contactos.jsx
+++ src/Contactos.jsx
@@ -1,13 +1,66 @@
-// Contactos.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt, faPhone, faEnvelope, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const Contactos = () => {
-  return (
-    <div className="container mt-5">
-      <h1 className="text-center mb-4">Lista de Contactos (por implementar)</h1>
-      {/* Puedes agregar aquí la lógica para mostrar la lista de contactos */}
-    </div>
-  );
+    // Estado con valores genéricos
+    const [contactos, setContactos] = React.useState([
+        {
+            id: 1,
+            nombre: 'John Doe',
+            direccion: '123 Main St',
+            telefono: '555-1234',
+            email: 'john@example.com'
+        },
+        {
+            id: 2,
+            nombre: 'Jane Doe',
+            direccion: '456 Oak St',
+            telefono: '555-5678',
+            email: 'jane@example.com'
+        }
+    ]);
+
+    return (
+        <div className="container mt-5">
+            {/* Botón para agregar nuevo contacto */}
+            <Link to="/nuevo-contacto" className="btn btn-success float-end mb-3">
+                Añadir Nuevo Contacto
+            </Link>
+
+            {/* Lista de contactos */}
+            {contactos.map((contacto, index) => (
+                <div key={index} className="mb-3 border border-light">
+                    <div className="d-flex justify-content-between align-items-center mb-2">
+                        <strong>{contacto.nombre}</strong>
+                        <div>
+                            <FontAwesomeIcon icon={faEdit} className="me-2" />
+                            <FontAwesomeIcon icon={faTrash} />
+                        </div>
+                    </div>
+                    <div>
+                        <img src="https://placekitten.com/180/180" alt="Contacto" className="rounded-circle" />
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faMapMarkerAlt} className="me-2" />
+                        {contacto.direccion}
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faPhone} className="me-2" />
+                        {contacto.telefono}
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faEnvelope} className="me-2" />
+                        {contacto.email}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Contactos;
+
+
+
